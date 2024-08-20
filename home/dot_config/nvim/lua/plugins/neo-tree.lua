8d083b6b5ad9602a75d49cc5bc7a0bb8741f0e19
tyr <tyrposting@icloud.com>
2024-08-20 11:50:19 -0700

    feat: add neovim config

---
+++ home/dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+    window = {
+      mappings = {
+        ["<tab>"] = "open",
+      },
+    },
+  },
+}
